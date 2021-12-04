a6f012f6f7463ee510537f8ea1534ee835e2d52c
garvit170 <garvitgulati47@gmail.com>
2021-12-04 16:35:31 +0530

    server setup

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app=express();
+const port = 4000;
+
+//================ Static Files ==================
+app.use(express.static('./assets'));
+
+//======== use express router ===============
+app.use('/',require('./routes'));
+
+app.listen(port,function(err){
+    if(err){
+        console.log(`Error encountered in running the server: ${err}`);     //interpolation string
+        return;
+    }
+    console.log(`Server is running on port: ${port}`);
+});
