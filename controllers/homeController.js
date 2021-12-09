abfe58e20b666979a349fe872c0200d144efb70c
Tushar Mishra <tusharmishra020@gmail.com>
2021-12-09 12:50:40 +0530

    initiative

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -2,10 +2,6 @@ module.exports.home = function(req,res){
     return res.render('index');
 }
 
-module.exports.initiatives = function(req,res){
-    return res.render('initiative-page');
-}
-
 module.exports.about = function(req,res){
     return res.render('about');
 }
